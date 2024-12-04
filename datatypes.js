18641c0eb955467f241facea3672fdc64ac68507
Tabitha-Twitchit <tabitha@tabithanikolai.com>
2024-12-03 21:40:31 -0600

    In data types

---
+++ datatypes.js
@@ -0,0 +1,28 @@
+/**
+ * 0: Data types are ways of categorizing information to create taxonomies of behavior / use. They correspond to prototype objects 
+ * that have specific prototype methods attached to them that facilitate manipulation of the given data. They are often divided 
+ * between simple and complex types.
+ * 
+ * 1: A number data type can be positive or negative. Can be an integer or a float (decimal). Numbers can only be of a certain 
+ * size before they must be represented by approximation or special notation. A simple data type.
+ * 
+ * 2: A series of alphabetic, numeric, or symbolic characters. Strings are denoted through being surrounded by either single 
+ * or double quotes. Numeric characters can't be mathematically operated without conversion to number datat type. Strings have
+ * their own specific methods of manipulation. Strings are immutable, string methods create newstrings based on the current string
+ *  Each character of a string corresponds to a numeric index starting with 0. A simple data type.
+ * 
+ * 3: Booleans provide a binary system of analysis--true or false. usually used to handle conditional logic. A simple data type.
+ * 
+ * 4: Arrays are a complex data type. They are comprised of an indexed series of elements, beginning at 0. Arrays are denoted 
+ * by square brackets, with elements divided by commas. Arrays can homogenous or heterogenous in terms of composition, but are 
+ * primarily homogenous (comprised of one datat type). Arrays can contain both simple and / or complex data types, including more
+ * arrays. An array of arrays is called a matrix.
+ * 
+ * 5: An object is another complex data type, but rather than an indexed list it is comprised of properties--pairs wherein a descriptive
+ *  key string corresponds to a value of any type. Objects can have both different data types and functions (methods) as properties 
+ * of it. 
+ * 
+ * 6: A function is itself a data type/
+ */
+
+// 1: 
